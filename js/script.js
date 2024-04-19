ec8f556488ed64d3ff9328ad102596c2e420bbdf
Derek Myers <cashmerelc@gmail.com>
2024-04-19 10:59:32 +0200

    Add event listener for show answer button to hide or show the answer and update button text accordingly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+/* ----------------------------------------------------------------
+                        Bookmarks
+---------------------------------------------------------------- */
+
 // Get first bookmark image on the index page
 const bookmark = document.querySelector(".first-question img");
-// console.log(bookmark.getAttribute("src"));
 
+// Establish all bookmarks are not filled
 let bookmarkChecked = false;
 
+// Change bookmark image based on whether it is already filled or not
 bookmark.addEventListener("click", () => {
   if (!bookmarkChecked) {
     bookmark.setAttribute("src", "./images/bookmark_filled.png");
@@ -11,5 +16,33 @@ bookmark.addEventListener("click", () => {
   } else {
     bookmark.setAttribute("src", "./images/bookmark.png");
     bookmarkChecked = false;
+  }
+});
+
+/* ----------------------------------------------------------------
+                        Answers
+---------------------------------------------------------------- */
+
+// Get first answer button on index page
+const toggleAnswerButton = document.querySelector(
+  ".first-question .btn-show-answer"
+);
+
+// Get first answer on index page
+const answer = document.querySelector(".first-question .answer");
+
+// Establish the answer is not visible
+let answerVisible = false;
+
+// If answer is hidden, make it visible and change button text. Otherwise hide the answer
+toggleAnswerButton.addEventListener("click", () => {
+  if (!answerVisible) {
+    answer.style.visibility = "visible";
+    toggleAnswerButton.textContent = "Hide Answer";
+    answerVisible = true;
+  } else {
+    answer.style.visibility = "hidden";
+    toggleAnswerButton.textContent = "Show Answer";
+    answerVisible = false;
   }
 });
